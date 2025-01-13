c8d1e661fb91f1f57ff0434b3fb694acc0214b53
Göksu Ayaz <goksuayaz79@gmail.com>
2025-01-13 16:13:32 +0300

    Initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,37 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import Colors from '@/constants/Colors';
+import products from '@/assets/data/products';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+const product = products[0];
+
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={{ fontSize: 18, fontWeight: '700', marginVertical: 10 }}> {product.name} </Text>
+      <Text style={styles.price}>${product.price} </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 20
+
+
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold'
+
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    borderRadius: 16
+  }
+
 });
---
+++ assets/data/products.ts
@@ -0,0 +1,74 @@
+const products = [
+    {
+        id: 1,
+        name: 'Italiano',
+        image:
+            'https://cdn.dpeurasia.com/dms/images/product/TR_497e198b-edcc-4d18-ac35-3e17e4f69521_1170x400.jpg',
+        price: 12.99,
+    },
+    {
+        id: 2,
+        name: 'Mexicano',
+        image:
+            'https://cdn.dpeurasia.com/dms/images/product/TR_26952357-bfd0-4c68-935f-27df8aa03bfd_1170x400.jpg',
+        price: 14.99,
+    },
+    {
+        id: 3,
+        name: 'Peperonni',
+        image:
+            'https://cdn.dpeurasia.com/dms/images/product/TR_9c438515-5e50-4672-a22e-e2ea825e5419_1170x400.jpg',
+        price: 13.47,
+    },
+    {
+        id: 4,
+        name: 'Margarita',
+        image:
+            'https://cdn.dpeurasia.com/dms/images/product/TR_4f2e0866-039f-4418-8c30-5853afac9476_1170x400.jpg',
+        price: 9.9,
+    },
+    {
+        id: 5,
+        name: 'Vegi',
+        image:
+            'https://cdn.dpeurasia.com/dms/images/product/TR_6e328fa1-b44c-430f-81f5-42bc4903f0b1_1170x400.jpg',
+        price: 12.99,
+    },
+    {
+        id: 6,
+        name: 'Superix',
+        image:
+            'https://cdn.dpeurasia.com/dms/images/product/TR_0ef389cd-08c2-48e0-a1b5-1255beaa5ec4_1170x400.jpg',
+        price: 11.49,
+    },
+    {
+        id: 7,
+        name: 'Kantin',
+        image:
+            'https://cdn.dpeurasia.com/dms/images/product/TR_642ae060-4f58-416f-9b36-5574d5005862_1170x400.jpg',
+        price: 16.99,
+    },
+    {
+        id: 8,
+        name: 'Callypso',
+        image:
+            'https://cdn.dpeurasia.com/dms/images/product/TR_715a2a73-b7dc-4398-9411-a65aa5469865_1170x400.jpg',
+        price: 12.89,
+    },
+    {
+        id: 9,
+        name: 'Extravaganzza',
+        image:
+            'https://cdn.dpeurasia.com/dms/images/product/TR_4573bb83-29ab-4234-b3b7-5298d99234c6_1170x400.jpg',
+        price: 13.99,
+    },
+    {
+        id: 10,
+        name: '3 Cheese',
+        image:
+            'https://cdn.dpeurasia.com/dms/images/product/TR_bd512acc-97c4-4e86-be42-83ba4e92f197_1170x400.jpg',
+        price: 13.29,
+    },
+];
+
+export default products;
