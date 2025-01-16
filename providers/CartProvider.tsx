1871fe6223ee8ad76343fdb6379d90723968f857
Göksu Ayaz <goksuayaz79@gmail.com>
2025-01-16 16:21:02 +0300

    Shopping Cart Details

--- providers/CartProvider.tsx
+++ providers/CartProvider.tsx
@@ -7,12 +7,14 @@ type CartType = {
     items: CartItem[];
     addItem: (product: Product, size: CartItem["size"]) => void;
     updateQuantity: (itemId: string, amount: -1 | 1) => void;
+    total: number;
 }
 
 const CartContext = createContext<CartType>({
     items: [],
     addItem: () => { },
     updateQuantity: () => { },
+    total: 0,
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -20,7 +22,12 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     const [items, setItems] = useState<CartItem[]>([]);
 
     const addItem = (product: Product, size: CartItem["size"]) => {
+        const existingItem = items.find(item => item.product === product && item.size === size)
 
+        if (existingItem) {
+            updateQuantity(existingItem.id, 1);
+            return;
+        }
 
         const newCartItem: CartItem = {
             id: randomUUID(),
@@ -38,15 +45,14 @@ const CartProvider = ({ children }: PropsWithChildren) => {
         setItems(
             items.map((item) =>
                 item.id !== itemId ? item : { ...item, quantity: item.quantity + amount }
-            )
+            ).filter((item) => item.quantity > 0)
         )
     }
 
-    console.log(items)
-
+    const total = items.reduce((sum, item) => (sum + item.product.price * item.quantity), 0);
     return (
 
-        <CartContext.Provider value={{ items, addItem, updateQuantity }} >
+        <CartContext.Provider value={{ items, addItem, updateQuantity, total }} >
             {children}
         </CartContext.Provider>
     )
