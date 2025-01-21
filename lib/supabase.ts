0ac6b4f6cb99ab40d7b04b490a685f924f065cd2
Göksu Ayaz <goksuayaz79@gmail.com>
2025-01-21 22:27:28 +0300

    add supabase infos

---
+++ lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+    getItem: (key: string) => {
+        return SecureStore.getItemAsync(key);
+    },
+    setItem: (key: string, value: string) => {
+        SecureStore.setItemAsync(key, value);
+    },
+    removeItem: (key: string) => {
+        SecureStore.deleteItemAsync(key);
+    },
+};
+
+const supabaseUrl = 'https://ilduknaslywelxlsuimb.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsZHVrbmFzbHl3ZWx4bHN1aW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc0ODcwNzksImV4cCI6MjA1MzA2MzA3OX0.Et4BuPsiq-JmoF71y3801fnes4UC5EzxKkIByY_nd8U';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        storage: ExpoSecureStoreAdapter as any,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
