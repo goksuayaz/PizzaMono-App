bcf7852749b5be2c15a9078386e7addd6931cb0c
Göksu Ayaz <goksuayaz79@gmail.com>
2025-01-23 16:19:29 +0300

    Admin Side Authentication

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -5,32 +5,50 @@ import { createContext, PropsWithChildren, useContext, useEffect, useState } fro
 
 type AuthData = {
     session: Session | null;
+    profile: any;
     loading: boolean;
+    isAdmin: boolean;
 };
 
 const AuthContext = createContext<AuthData>({
     session: null,
     loading: true,
+    profile: null,
+    isAdmin: false,
 
 });
 
 export default function AuthProvider({ children }: PropsWithChildren) {
     const [session, setSession] = useState<Session | null>(null);
+    const [profile, setProfile] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchSession = async () => {
-            const { data } = await supabase.auth.getSession();
-            setSession(data.session);
+            const { data: { session },
+            } = await supabase.auth.getSession();
+            setSession(session);
+
+
+            if (session) {
+                const { data } = await supabase
+                    .from('profiles')
+                    .select('*')
+                    .eq('id', session.user.id)
+                    .single();
+                setProfile(data || null);
+            }
             setLoading(false);
         }
+
         fetchSession();
         supabase.auth.onAuthStateChange((_event, session) => {
             setSession(session);
         })
     }, []);
 
-    return (<AuthContext.Provider value={{ session, loading }}>{children}</AuthContext.Provider>
+    return (<AuthContext.Provider value={{ session, loading, profile, isAdmin: profile?.group === "ADMIN" }}>
+        {children}</AuthContext.Provider>
 
     )
 }
