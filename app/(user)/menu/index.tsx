529815b57ceca888eae315cb48127e1b137cb96d
Göksu Ayaz <goksuayaz79@gmail.com>
2025-01-23 17:30:42 +0300

    crud operations

--- app/(user)/menu/index.tsx
+++ app/(user)/menu/index.tsx
@@ -1,19 +1,38 @@
-import { View, FlatList } from 'react-native';
-import products from '@assets/data/products';
+import { ActivityIndicator, FlatList, Text } from 'react-native';
 import ProductListItem from '@components/ProductListItem';
+import { useProductList } from '@/api/products';
+
 
 export default function MenuScreen() {
+  const { data: products, error, isLoading } = useProductList();
+
+  if (isLoading) {
+    return <ActivityIndicator />
+  }
+
+  if (error) {
+    return <Text> Failed to fetch products</Text>
+  }
+
+
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const { data, error } = await supabase.from('products').select('*');
+  //     console.log(error);
+  //     console.log(data);
+  //   };
+  //   fetchProducts();
+  // }, [])
+
+
   return (
     <FlatList
       data={products} renderItem={({ item }) => <ProductListItem product={item} />}
       numColumns={2}
-      contentContainerStyle={{ gap: 5 }}
-      columnWrapperStyle={{ gap: 5 }}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
     />
 
   )
 
 }
-
-
-
